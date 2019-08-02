0dc7d7fcd8a0f314368a6c9121e8fee83ca21eeb
lixinquan <lxq1530862790@sina.com>
2019-08-02 14:41:10 +0800

    hystrix项目实践

---
+++ eureka-consumer-ribbon-hystrix/src/main/java/com/studycloud/controller/DcController.java
@@ -0,0 +1,24 @@
+package com.studycloud.controller;
+
+import com.studycloud.service.ConsumerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class DcController {
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+    @Autowired
+    RestTemplate restTemplate;
+    @Autowired
+    private ConsumerService consumerService;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return consumerService.consumer();
+    }
+}
